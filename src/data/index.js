1fcf0c029703b3b83972e55a1a3ae30fd013b028
leonelborgesdev <leonel03t@gmail.com>
2024-07-04 22:19:57 -0400

    data

---
+++ src/data/index.js
@@ -0,0 +1,340 @@
+export const data = [
+  {
+    name: {
+      common: "Wallis and Futuna",
+      official: "Territory of the Wallis and Futuna Islands",
+      nativeName: {
+        fra: {
+          official: "Territoire des îles Wallis et Futuna",
+          common: "Wallis et Futuna",
+        },
+      },
+    },
+    tld: [".wf"],
+    cca2: "WF",
+    ccn3: "876",
+    cca3: "WLF",
+    independent: false,
+    status: "officially-assigned",
+    unMember: false,
+    currencies: {
+      XPF: {
+        name: "CFP franc",
+        symbol: "₣",
+      },
+    },
+    idd: {
+      root: "+6",
+      suffixes: ["81"],
+    },
+    capital: ["Mata-Utu"],
+    altSpellings: [
+      "WF",
+      "Territory of the Wallis and Futuna Islands",
+      "Territoire des îles Wallis et Futuna",
+    ],
+    region: "Oceania",
+    subregion: "Polynesia",
+    languages: {
+      fra: "French",
+    },
+    latlng: [-13.3, -176.2],
+    landlocked: false,
+    area: 142,
+    demonyms: {
+      eng: {
+        f: "Wallis and Futuna Islander",
+        m: "Wallis and Futuna Islander",
+      },
+    },
+    flag: "🇼🇫",
+    maps: {
+      googleMaps: "https://goo.gl/maps/CzVqK74QYtbHv65r5",
+      openStreetMaps: "https://www.openstreetmap.org/relation/3412448",
+    },
+    population: 11750,
+    car: {
+      signs: ["F"],
+      side: "right",
+    },
+    timezones: ["UTC+12:00"],
+    continents: ["Oceania"],
+    flags: {
+      png: "https://flagcdn.com/w320/wf.png",
+      svg: "https://flagcdn.com/wf.svg",
+    },
+    coatOfArms: {},
+    startOfWeek: "monday",
+    capitalInfo: {
+      latlng: [-13.95, -171.93],
+    },
+    postalCode: {
+      format: "#####",
+      regex: "^(986\\d{2})$",
+    },
+  },
+  {
+    name: {
+      common: "Iceland",
+      official: "Iceland",
+      nativeName: {
+        isl: {
+          official: "Ísland",
+          common: "Ísland",
+        },
+      },
+    },
+    tld: [".is"],
+    cca2: "IS",
+    ccn3: "352",
+    cca3: "ISL",
+    cioc: "ISL",
+    independent: true,
+    status: "officially-assigned",
+    unMember: true,
+    currencies: {
+      ISK: {
+        name: "Icelandic króna",
+        symbol: "kr",
+      },
+    },
+    idd: {
+      root: "+3",
+      suffixes: ["54"],
+    },
+    capital: ["Reykjavik"],
+    altSpellings: ["IS", "Island", "Republic of Iceland", "Lýðveldið Ísland"],
+    region: "Europe",
+    subregion: "Northern Europe",
+    languages: {
+      isl: "Icelandic",
+    },
+    latlng: [65, -18],
+    landlocked: false,
+    area: 103000,
+    demonyms: {
+      eng: {
+        f: "Icelander",
+        m: "Icelander",
+      },
+      fra: {
+        f: "Islandaise",
+        m: "Islandais",
+      },
+    },
+    flag: "🇮🇸",
+    maps: {
+      googleMaps: "https://goo.gl/maps/WxFWSQuc3oamNxoE6",
+      openStreetMaps: "https://www.openstreetmap.org/relation/299133",
+    },
+    population: 366425,
+    gini: {
+      2017: 26.1,
+    },
+    fifa: "ISL",
+    car: {
+      signs: ["IS"],
+      side: "right",
+    },
+    timezones: ["UTC"],
+    continents: ["Europe"],
+    flags: {
+      png: "https://flagcdn.com/w320/is.png",
+      svg: "https://flagcdn.com/is.svg",
+      alt: "The flag of Iceland has a blue field with a large white-edged red cross that extends to the edges of the field. The vertical part of this cross is offset towards the hoist side.",
+    },
+    coatOfArms: {
+      png: "https://mainfacts.com/media/images/coats_of_arms/is.png",
+      svg: "https://mainfacts.com/media/images/coats_of_arms/is.svg",
+    },
+    startOfWeek: "monday",
+    capitalInfo: {
+      latlng: [64.15, -21.95],
+    },
+    postalCode: {
+      format: "###",
+      regex: "^(\\d{3})$",
+    },
+  },
+  {
+    name: {
+      common: "Luxembourg",
+      official: "Grand Duchy of Luxembourg",
+      nativeName: {
+        deu: {
+          official: "Großherzogtum Luxemburg",
+          common: "Luxemburg",
+        },
+        fra: {
+          official: "Grand-Duché de Luxembourg",
+          common: "Luxembourg",
+        },
+        ltz: {
+          official: "Groussherzogtum Lëtzebuerg",
+          common: "Lëtzebuerg",
+        },
+      },
+    },
+    tld: [".lu"],
+    cca2: "LU",
+    ccn3: "442",
+    cca3: "LUX",
+    cioc: "LUX",
+    independent: true,
+    status: "officially-assigned",
+    unMember: true,
+    currencies: {
+      EUR: {
+        name: "Euro",
+        symbol: "€",
+      },
+    },
+    idd: {
+      root: "+3",
+      suffixes: ["52"],
+    },
+    capital: ["Luxembourg"],
+    altSpellings: [
+      "LU",
+      "Grand Duchy of Luxembourg",
+      "Grand-Duché de Luxembourg",
+      "Großherzogtum Luxemburg",
+      "Groussherzogtum Lëtzebuerg",
+    ],
+    region: "Europe",
+    subregion: "Western Europe",
+    languages: {
+      deu: "German",
+      fra: "French",
+      ltz: "Luxembourgish",
+    },
+    latlng: [49.75, 6.16666666],
+    landlocked: true,
+    borders: ["BEL", "FRA", "DEU"],
+    area: 2586,
+    demonyms: {
+      eng: {
+        f: "Luxembourger",
+        m: "Luxembourger",
+      },
+      fra: {
+        f: "Luxembourgeoise",
+        m: "Luxembourgeois",
+      },
+    },
+    flag: "🇱🇺",
+    maps: {
+      googleMaps: "https://goo.gl/maps/L6b2AgndgHprt2Ko9",
+      openStreetMaps:
+        "https://www.openstreetmap.org/relation/2171347#map=10/49.8167/6.1335",
+    },
+    population: 632275,
+    gini: {
+      2018: 35.4,
+    },
+    fifa: "LUX",
+    car: {
+      signs: ["L"],
+      side: "right",
+    },
+    timezones: ["UTC+01:00"],
+    continents: ["Europe"],
+    flags: {
+      png: "https://flagcdn.com/w320/lu.png",
+      svg: "https://flagcdn.com/lu.svg",
+      alt: "The flag of Luxembourg is composed of three equal horizontal bands of red, white and light blue.",
+    },
+    coatOfArms: {
+      png: "https://mainfacts.com/media/images/coats_of_arms/lu.png",
+      svg: "https://mainfacts.com/media/images/coats_of_arms/lu.svg",
+    },
+    startOfWeek: "monday",
+    capitalInfo: {
+      latlng: [49.6, 6.12],
+    },
+    postalCode: {
+      format: "####",
+      regex: "^(\\d{4})$",
+    },
+  },
+  {
+    name: {
+      common: "Mali",
+      official: "Republic of Mali",
+      nativeName: {
+        fra: {
+          official: "République du Mali",
+          common: "Mali",
+        },
+      },
+    },
+    tld: [".ml"],
+    cca2: "ML",
+    ccn3: "466",
+    cca3: "MLI",
+    cioc: "MLI",
+    independent: true,
+    status: "officially-assigned",
+    unMember: true,
+    currencies: {
+      XOF: {
+        name: "West African CFA franc",
+        symbol: "Fr",
+      },
+    },
+    idd: {
+      root: "+2",
+      suffixes: ["23"],
+    },
+    capital: ["Bamako"],
+    altSpellings: ["ML", "Republic of Mali", "République du Mali"],
+    region: "Africa",
+    subregion: "Western Africa",
+    languages: {
+      fra: "French",
+    },
+    latlng: [17, -4],
+    landlocked: true,
+    borders: ["DZA", "BFA", "GIN", "CIV", "MRT", "NER", "SEN"],
+    area: 1240192,
+    demonyms: {
+      eng: {
+        f: "Malian",
+        m: "Malian",
+      },
+      fra: {
+        f: "Malienne",
+        m: "Malien",
+      },
+    },
+    flag: "🇲🇱",
+    maps: {
+      googleMaps: "https://goo.gl/maps/u9mYJkCB19wyuzh27",
+      openStreetMaps: "https://www.openstreetmap.org/relation/192785",
+    },
+    population: 20250834,
+    gini: {
+      2009: 33,
+    },
+    fifa: "MLI",
+    car: {
+      signs: ["RMM"],
+      side: "right",
+    },
+    timezones: ["UTC"],
+    continents: ["Africa"],
+    flags: {
+      png: "https://flagcdn.com/w320/ml.png",
+      svg: "https://flagcdn.com/ml.svg",
+      alt: "The flag of Mali is composed of three equal vertical bands of green, yellow and red.",
+    },
+    coatOfArms: {
+      png: "https://mainfacts.com/media/images/coats_of_arms/ml.png",
+      svg: "https://mainfacts.com/media/images/coats_of_arms/ml.svg",
+    },
+    startOfWeek: "monday",
+    capitalInfo: {
+      latlng: [12.65, -8],
+    },
+  },
+];
