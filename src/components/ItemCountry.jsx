e68566b37b97268141e40cddf5e8950423040833
leonelborgesdev <leonel03t@gmail.com>
2024-08-15 11:58:24 -0400

    datos del pais

--- src/components/ItemCountry.jsx
+++ src/components/ItemCountry.jsx
@@ -3,7 +3,13 @@ import { Text, View } from 'react-native'
 
 const ItemCountry = (props) => {
   return (
-    <View><Text>{props.name.common}</Text></View>
+    <View>
+        <Text>Nombre: {props.name.common}</Text>
+        <Text>Capital: {props.capital}</Text>
+        <Text>Continente: {props.region}</Text>
+        <Text>Poblacion: {props.population}</Text>
+        <Text>Area: {props.area} Km2</Text>
+    </View>
   )
 }
 
