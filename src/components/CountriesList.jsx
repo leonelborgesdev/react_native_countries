66c89ac0b75090891de1359a2e0310f925ff8499
leonelborgesdev <leonel03t@gmail.com>
2024-08-15 11:15:16 -0400

    Countries list

---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const CountriesList = () => {
+  return (
+    <View><Text>CountriesList</Text></View>
+  )
+}
+
+export default CountriesList
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,7 +4,10 @@ import { Text, View } from "react-native";
 export default function Menu() {
   return (
     <View>
-      <Text>Menu heloo</Text>
+      <Text>Hola Leooo</Text>
+      <Text>Hola </Text>
+      <Text>Hello</Text>
+      <Text>Hi!!</Text>
     </View>
   );
 }
