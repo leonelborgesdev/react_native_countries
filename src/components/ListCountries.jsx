786b526e4640a168de81209a4c20176da53578b6
leonelborgesdev <leonel03t@gmail.com>
2024-08-16 10:19:37 -0400

    fetchcountries

--- src/components/ListCountries.jsx
+++ src/components/ListCountries.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import { FlatList, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { FlatList, Text} from 'react-native'
 import { data } from "../data";
 import ItemCountry from './ItemCountry';
 
 const ListCountries = () => {
+  const [countries, setCountries]= useState();
+  const fetchCountries= async ()=>{
+    const api = `https://restcountries.com/v3.1/all`;
+    const response = await fetch(api);
+    const json = await response.json();
+    setCountries(json)
+  }
+  useEffect(()=>{
+    fetchCountries();
+    console.log("objCountries",countries)
+  },[])
   console.log(data)
   return (
     <FlatList
-    data={data}
+    data={countries}
     ItemSeparatorComponent={()=> <Text> </Text>}
     renderItem={({ item: country})=> <ItemCountry {...country}/>}
     />
