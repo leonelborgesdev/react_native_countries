63e64ee4f652fc8d2e96a4f50993ae77260ba94a
leonelborgesdev <leonel03t@gmail.com>
2024-08-16 09:39:43 -0400

    image

--- src/components/ItemCountry.jsx
+++ src/components/ItemCountry.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 
 const ItemCountry = (props) => {
   return (
     <View>
+      <View style={{paddingRight: 10}}>
+        <Image source={{ uri: props.flags.png}}/>
+      </View>
+      <View>
         <Text>Nombre: {props.name.common}</Text>
         <Text>Capital: {props.capital}</Text>
         <Text>Continente: {props.region}</Text>
         <Text>Poblacion: {props.population}</Text>
-        <Text>Area: {props.area} Km2</Text>
+        <Text>Area: {props.area} Km2</Text></View>
     </View>
   )
 }
