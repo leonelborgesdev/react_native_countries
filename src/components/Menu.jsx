f61eda1d4536fa01ea7c0f12e92dbce2754903a5
leonelborgesdev <leonel03t@gmail.com>
2024-08-27 11:48:01 -0400

    Buttons

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,8 +4,19 @@ import ListCountries from "./ListCountries";
 
 export default function Menu() {
   return (
-    <View style={{alignItems: 'center'}}>
-      <Text style={{fontSize: 20}}>Bienvenido a AppCountries</Text>
+    <View>
+      <View style={{alignItems: 'center'}}>
+        <Text style={{fontSize: 20}}>Bienvenido a AppCountries</Text>
+      </View>
+    <View >
+      <Text style={{fontSize: 20}}>Banderas y Paises</Text>
+    </View>
+    <View >
+      <Text style={{fontSize: 20}}>Paises y Capitales</Text>
+    </View>
+    <View >
+      <Text style={{fontSize: 20}}>Paises y Continentes</Text>
+    </View>
     </View>
   );
 }
