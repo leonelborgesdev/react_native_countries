964393423a90dcdc6131fd6f6a887d1a9446bacf
leonelborgesdev <leonel03t@gmail.com>
2024-08-28 22:54:08 -0400

    flagscountries

---
+++ src/components/FlagsCountries/FlagsCountries.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const FlagsCountries = () => {
+  return (
+    <View>
+        <Text>FlagsCountries</Text>
+    </View>
+  )
+}
+
+export default FlagsCountries
