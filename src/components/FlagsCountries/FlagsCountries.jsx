ca0b4bf46a12ba7e30b025455807ffe9cf23f9aa
leonelborgesdev <leonel03t@gmail.com>
2024-08-28 23:13:47 -0400

    styles button region

--- src/components/FlagsCountries/FlagsCountries.jsx
+++ src/components/FlagsCountries/FlagsCountries.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, TouchableWithoutFeedback, View } from 'react-native'
+import { Link } from 'react-router-native'
 
 const FlagsCountries = () => {
   return (
     <View>
+      <View>
         <Text>FlagsCountries</Text>
+      </View>
+      <View component={TouchableWithoutFeedback} >
+        <Link to={'/'}>
+          <Text style={{fontSize: 20}}>Volver...</Text>
+        </Link>
+      </View>
     </View>
   )
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 import ListCountries from "./ListCountries";
+import { Link } from "react-router-native";
 
 const styles= StyleSheet.create({
   buttonFlags: {
@@ -14,24 +15,32 @@ const styles= StyleSheet.create({
     width: 150,
     alignItems: 'center',
     height: 50
+  },
+  buttonRegion:{
+    backgroundColor: 'orange',
+    width: 150,
+    alignItems:'center',
+    height: 50
   }
 })
 
 export default function Menu() {
   return (
-    <View>
+    <View style={{gap: 25, padding: 5}}>
       <View style={{alignItems: 'center'}}>
         <Text style={{fontSize: 20}}>Bienvenido a AppCountries</Text>
       </View>
-    <View component={TouchableWithoutFeedback} style={styles.buttonFlags}>
-      <Text style={{fontSize: 20}}>Banderas y Paises</Text>
-    </View>
-    <View component={TouchableWithoutFeedback} style={styles.buttonCountries}>
-      <Text style={{fontSize: 20}}>Paises y Capitales</Text>
-    </View>
-    <View component={TouchableWithoutFeedback} style={styles.buttonRegion}>
-      <Text style={{fontSize: 20}}>Paises y Continentes</Text>
-    </View>
+      <View component={TouchableWithoutFeedback} style={styles.buttonFlags}>
+        <Link to={'/flagscountries'}>
+          <Text style={{fontSize: 20}}>Banderas y Paises</Text>
+        </Link>
+      </View>
+      <View component={TouchableWithoutFeedback} style={styles.buttonCountries}>
+        <Text style={{fontSize: 20}}>Paises y Capitales</Text>
+      </View>
+      <View component={TouchableWithoutFeedback} style={styles.buttonRegion}>
+        <Text style={{fontSize: 20}}>Paises y Continentes</Text>
+      </View>
     </View>
   );
 }
